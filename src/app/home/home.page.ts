f8d22819c290c00299767e88182d4908afddb00c
unknown <mail@robingenz.de>
2019-01-20 15:03:21 +0100

    add ion-segment and ion-slides

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
 export class HomePage {
+  segment: number;
+  @ViewChild("slides") slides;
 
+  constructor() {
+    this.segment = 0;
+  }
+
+  public async setSegment(activeIndex: Promise<number>) {
+    this.segment = await activeIndex;
+  }
 }
